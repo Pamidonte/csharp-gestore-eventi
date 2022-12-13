2802faccee4f094ab91a666ab3ae4dc08e227350
Pamidonte <francescomarrone8@gmail.com>
2022-12-13 10:25:22 +0100

    Aggiungere i file di progetto.

---
+++ GestoreEventi/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi
+{
+    internal class Evento
+    {
+        string titolo;
+        int data;
+        int capienzaMassima;
+        int numeroPostiPrenotati;
+    }
+}
