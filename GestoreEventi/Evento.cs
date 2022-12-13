4b3a938f56c08719fb4e8462afc08331c353eac8
Pamidonte <francescomarrone8@gmail.com>
2022-12-13 12:44:50 +0100

    push su richiesta

--- GestoreEventi/Evento.cs
+++ GestoreEventi/Evento.cs
@@ -8,9 +8,49 @@ namespace GestoreEventi
 {
     internal class Evento
     {
-        string titolo;
-        int data;
-        int capienzaMassima;
-        int numeroPostiPrenotati;
+       private string titolo;
+       public string data;
+       public  int capienzaMassima;
+       public int numeroPostiPrenotati;
+
+        // setters
+        public void SetTitolo(string titolo)
+        {
+          if (string.IsNullOrEmpty(titolo))
+            {
+                throw new Exception("il titolo dell evento non puo essere vuoto");
+            }
+        }
+        public void SetData(string data)
+        {
+         if (string.IsNullOrEmpty(data))
+            {
+                throw new Exception("la data non è stata inserita");
+            }
+        }
+
+
+
+        // getters
+        public string GetTitolo()
+        { return this.titolo; }
+
+        public string GetData()
+        { return this.data; }
+
+        public int GetCapienzaMassima()
+        { return this.capienzaMassima; }
+
+        public int GetNumeroPostiPrenotati()
+        { return this.numeroPostiPrenotati; }
+
+        // costruttore
+        public Evento(string titolo, string data, int capienzaMassima, int numeroPostiPrenotati)
+        {
+            SetTitolo(titolo);
+            SetData(data);
+            this.capienzaMassima = capienzaMassima;
+            this.numeroPostiPrenotati = numeroPostiPrenotati;
+        }
     }
 }
